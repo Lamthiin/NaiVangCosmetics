b4caa7436cae3eac3d2afbfaa712125a9e8985b4
LamThuyenNguyen <nguyenngoclamthuyen0977499533@gmail.com>
2025-12-11 08:13:36 +0700

    hien thi trang loi

--- src/main/java/demoweb/demo/error/PageFaultController.java
+++ src/main/java/demoweb/demo/error/PageFaultController.java
@@ -1,5 +1,6 @@
 package demoweb.demo.error;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,22 +8,23 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-public class PageFaultController {
+public class PageFaultController implements ErrorController {
 
-    @RequestMapping("/page-fault")
+    @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
+
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
+        int statusCode = 500;
+
         if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
-            if (statusCode == 404) {
-                return "Customer/PageFault404";
-            } else if (statusCode == 500) {
-                return "Customer/PageFault500";
-            } else if (statusCode == 403) {
-                return "Customer/PageFault403";
-            }
+            try {
+                statusCode = Integer.parseInt(status.toString());
+            } catch (Exception ignored) {}
         }
-        return "Customer/PageFault404";
+
+        if (statusCode == 404) return "Customer/PageFault404";
+        if (statusCode == 403) return "Customer/PageFault403";
+        return "Customer/PageFault500";
     }
 }
